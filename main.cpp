1e988694b7ddf977a088b4f16d7b83bfdfddec78
Sebastian <sebastians9517@gmail.com>
2023-08-18 02:10:45 -0500

    declared window height and width const variables and initialized window function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <raylib.h>
 
 int main (){
+    // Declaring window dimensions
+    const int window_width = 1280;
+    const int window_height = 720;
+    // Initializing window
+    InitWindow(window_width, window_height, "Dasher Game");
 
+
+    
+    
 }
