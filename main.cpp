e52af6dfce92c2a4d6f3981de418f352ccc22433
Sebastian <sebastians9517@gmail.com>
2023-08-18 04:07:57 -0500

    added position and velocity variables and rectangle dimensions

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <raylib.h>
 
 int main (){
-    // Declaring window dimensions
+    // Window dimensions
     const int window_width = 1280;
     const int window_height = 720;
+    // Rectangle dimensions
+    const int rectangle_width = 50;
+    const int rectangle_height = 80;
+    // Position and velocity
+    int position_Y = 0;
+    int velocity = 10; // This equals to 10px/frame
     // Initializing window
     InitWindow(window_width, window_height, "Dasher Game");
 
@@ -12,6 +18,8 @@ int main (){
         // Initializing canvas
         BeginDrawing();
         ClearBackground(WHITE);
+
+        position_Y += velocity;
         EndDrawing();
     }
     CloseWindow();
