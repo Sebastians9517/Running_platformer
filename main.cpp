aca3685f1a4dbb5093b15487a66646cbc42b4c5d
Sebastian <sebastians9517@gmail.com>
2023-08-29 04:42:04 -0500

    fixed texture importing issue by moving Texture2D, Rectangle and Vector2 variables after WindowInit

--- main.cpp
+++ main.cpp
@@ -5,31 +5,33 @@ int main (){
     const int window_width = 1280;
     const int window_height = 720;
 
+    // Downward acceleration due to gravity, given in pixels/frame^2
+    const double gravity = 0.981;
+
+
+
+    // Position and velocity
+    
+    double velocity = 0; // This is given in pixels/frame
+    const int jump_velocity = -22;
+    bool in_the_air{}; // Braced initialization defaults to a false value
+
+    // Initializing window
+    InitWindow(window_width, window_height, "Dasher Game");
     // Importing and setting up character sprite hitbox dimensions and position in map
-    Texture2D character = LoadTexture ("textures\scarfy.png");
+    // We import textures after InitWindow, which creates a conection with the GPU so memory can be accessed
+    Texture2D character = LoadTexture("textures/scarfy.png");
+    // Setting up the hitbox rectangle size
     Rectangle character_hitbox;
     character_hitbox.width = character.width/6;
     character_hitbox.height = character.height;
     character_hitbox.x = 0;
     character_hitbox.y = 0;
+    // Setting up sprite position in regards to the map
     Vector2 character_position;
     character_position.x = window_width/2 - character_hitbox.width/2;
-    character_hitbox.y = window_height - character_hitbox.height;
+    character_position.y = window_height - character_hitbox.height;
 
-    // Rectangle dimensions
-    const int rectangle_width = 50;
-    const int rectangle_height = 80;
-
-    // Position and velocity
-    int position_Y = (window_height - rectangle_height);
-    double velocity = 0; // This is given in pixels/frame
-    const int jump_velocity = -22;
-    bool in_the_air{}; // Braced initialization defaults to a false value
-    // Initializing window
-    InitWindow(window_width, window_height, "Dasher Game");
-
-    // Gravity acceleration due to gravity, given in pixels/frame^2
-    const double gravity = 0.981;
 
     SetTargetFPS(60);
     while (!WindowShouldClose()){
@@ -37,7 +39,7 @@ int main (){
         BeginDrawing();
         ClearBackground(WHITE);
         // Ensuring we are not already at ground level
-        if((position_Y >= window_height - rectangle_height)){
+        if((character_position.y >= window_height - character_hitbox.height)){
             velocity = 0;
             in_the_air = false;
         } else { 
@@ -52,11 +54,12 @@ int main (){
         } 
 
         // Updating position
-        position_Y += velocity;
+        character_position.y += velocity;
         
-        DrawRectangle(window_width/2, position_Y, rectangle_width, rectangle_height, BLUE);
+        DrawTextureRec(character, character_hitbox, character_position, WHITE);
         EndDrawing();
     }
+    UnloadTexture(character);
     CloseWindow();
     
     
